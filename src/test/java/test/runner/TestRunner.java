7197c11b7e302bf1a4fd802518e6af55816ee171
Sakharam Thorat <sakharam.thorat@vonage.com>
2024-01-21 11:28:55 +0530

    remove unused imports from TestRunner

--- src/test/java/test/runner/TestRunner.java
+++ src/test/java/test/runner/TestRunner.java
@@ -1,10 +1,6 @@
 package test.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.slf4j.Logger;
-
-import static org.slf4j.LoggerFactory.getLogger;
-
 @CucumberOptions(
         features= {"classpath:features"},
         glue="io.swagger.petstore.stepdefinitions",
@@ -12,6 +8,4 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 )
 
-public class TestRunner extends AbstractTestNGCucumberTests {
-    private static final Logger log = getLogger(TestRunner.class);
-}
+public class TestRunner extends AbstractTestNGCucumberTests { }
